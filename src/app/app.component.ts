d100ccd7c0e6e18ad6180331ea7f379824529e16
MarkDdCullen <MarkDdCullen+GitHub@proton.me>
2024-04-29 03:02:59 +0100

    Update Remember

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component, NgModule, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HttpClientModule, FormsModule, CommonModule] // Add FormsModule here
+  imports: [HttpClientModule, FormsModule, CommonModule]
 })
+
 export class AppComponent implements OnInit {
   hymns: any[] = [];
   currentHymnIndex: number = 0;
@@ -18,16 +20,20 @@ export class AppComponent implements OnInit {
   searchValue: string = '';
   @ViewChild('hymnNumberInput', { static: true }) hymnNumberInput!: ElementRef;
 
-  constructor(private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
+    const savedHymnIndex = localStorage.getItem('currentHymnIndex');
+    if (savedHymnIndex !== null) {
+      this.currentHymnIndex = +savedHymnIndex;
+    }
+
     // Make an HTTP GET request to fetch the hymns.json file
     this.http.get<any[]>('assets/hymns.json').subscribe(
       (data) => {
         this.hymns = data; // Assign the fetched data to the hymns variable
-        this.currentHymn = this.hymns[this.currentHymnIndex];
+        this.loadCurrentHymn();
         console.log('Hymns:', this.hymns); // Log the fetched hymns to the console
-        this.updateSearchPlaceholder();
       },
       (error) => {
         console.error('Error fetching hymns:', error); // Log any errors
@@ -35,11 +41,18 @@ export class AppComponent implements OnInit {
     );
   }
 
+  loadCurrentHymn() {
+    if (this.currentHymnIndex >= 0 && this.currentHymnIndex < this.hymns.length) {
+      this.currentHymn = this.hymns[this.currentHymnIndex];
+      this.updateSearchPlaceholder();
+    }
+  }
+
   incrementHymn() {
     if (this.currentHymnIndex < this.hymns.length - 1) {
       this.currentHymnIndex++;
-      this.currentHymn = this.hymns[this.currentHymnIndex];
-      this.updateSearchPlaceholder();
+      this.loadCurrentHymn();
+      this.saveCurrentHymnIndex();
       this.hymnNumberInput.nativeElement.blur(); // Unfocus the input field
     }
   }
@@ -47,8 +60,8 @@ export class AppComponent implements OnInit {
   decrementHymn() {
     if (this.currentHymnIndex > 0) {
       this.currentHymnIndex--;
-      this.currentHymn = this.hymns[this.currentHymnIndex];
-      this.updateSearchPlaceholder();
+      this.loadCurrentHymn();
+      this.saveCurrentHymnIndex();
       this.hymnNumberInput.nativeElement.blur(); // Unfocus the input field
     }
   }
@@ -57,9 +70,9 @@ export class AppComponent implements OnInit {
     const index = parseInt(this.searchValue, 10) - 1;
     if (!isNaN(index) && index >= 0 && index < this.hymns.length) {
       this.currentHymnIndex = index;
-      this.currentHymn = this.hymns[this.currentHymnIndex];
+      this.loadCurrentHymn();
+      this.saveCurrentHymnIndex();
       this.searchValue = ''; // Clear the search input after searching
-      this.updateSearchPlaceholder();
       this.hymnNumberInput.nativeElement.blur(); // Unfocus the input field
     }
   }
@@ -67,5 +80,9 @@ export class AppComponent implements OnInit {
   updateSearchPlaceholder() {
     // Update the placeholder attribute without changing searchValue
     this.hymnNumberInput.nativeElement.placeholder = (this.currentHymnIndex + 1).toString();
+  }
+
+  saveCurrentHymnIndex() {
+    localStorage.setItem('currentHymnIndex', this.currentHymnIndex.toString());
   }
 }
